8c58158187207d574407c8535e0b7a1af6b2e16c
Heitorado <hscs@cin.ufpe.br>
2018-08-23 21:54:51 -0300

    Add editing user profile

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'questions/answered' => 'questions#answered'
   get 'questions/pending'=> 'questions#pending'
-  #resources :users
+  resources :users, only: [:edit, :update]
   resources :questions do
     member do
       get 'answer'
